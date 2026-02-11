8dfa1187e4ccd7b09717b24fff54ff9b6c74663c
KALYAN CHAKRABORTY <154689485+Kalyan9064@users.noreply.github.com>
2026-02-11 20:10:22 +0000

    add nltk.text and fix nltk download

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import streamlit as st
 import pickle
 import string
-import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+import nltk
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
 
 # ---------------- PAGE CONFIG ----------------
 st.set_page_config(
